366769e5671b30b47b69cddb88a2a1dd88aad42a
Donghan Park <donghan.park@duke.edu>
2021-10-23 15:00:44 -0400

    added frontend framework

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # goodjobs
+
+## Team
+James Kim, Muthu Arivoli, Andrew Lee, Isaac Fan, Donghan Park
---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Login from './pages/Login'
+
+function App() {
+  return (
+    <div className="App">
+        <Router>
+            <Switch>
+                <Route path="/" exact component={Login}/>
+            </Switch>
+        </Router>
+    </div>
+  );
+}
+
+export default App;
