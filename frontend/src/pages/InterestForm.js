af0c223a1082941580a7319830bd28dbf7a93fe0
Donghan Park <donghan.park@duke.edu>
2021-10-23 15:42:07 -0400

    added login page

---
+++ frontend/src/pages/InterestForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const InterestForm = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default InterestForm
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import '../stylesheets/Login.scss'
+import InputField from './InputField'
 
 const Login = () => {
     return (
-        <div>
+        <div className='login-page'>
             <h1>WELCOME TO GOODJOBS</h1>
+            <InputField placeholder='Username' iconType='user'/>
+            <InputField placeholder='Password' iconType='pass'/>
+            <button>LOGIN</button>
+            <p>Or Signup</p>
         </div>
     )
 }
